90c194339afe31ecc0657bc42f53061875f002b0
AnthonyKafer <anthony.vieira.lino@gmail.com>
2026-05-19 22:25:11 -0300

    refactor

---
+++ csharp.NUnit/GildedRose/QualityCalculators/AgedBrieCalculator.cs
@@ -0,0 +1,18 @@
+﻿using GildedRoseKata.QualityCalculators.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRoseKata.QualityCalculators
+{
+    public class AgedBrieCalculator : IItemQualityCalculator
+    {
+        public void Calculate(Item item)
+        {
+            int quality = item.Quality;
+            if (item.Quality < 50) quality++;
+            if (item.SellIn < 0) quality++;
+            item.Quality = quality > 50 ? 50 : quality;
+        }
+    }
+}
---
+++ csharp.NUnit/GildedRose/QualityCalculators/SulfuraCalculator.cs
@@ -0,0 +1,15 @@
+﻿using GildedRoseKata.QualityCalculators.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GildedRoseKata.QualityCalculators
+{
+    public class SulfuraCalculator : IItemQualityCalculator
+    {
+        public void Calculate(Item item)
+        {
+            return;
+        }
+    }
+}
